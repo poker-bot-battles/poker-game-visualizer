be461bba42f705ab7ef8ad32d02b093c4106fdf8
Christian Alexander Hetling <chrhe@itu.dk>
2023-03-13 03:05:33 +0100

    remove controlPanel

--- poker-tournament-visualization/src/environments/environment.prod.ts
+++ poker-tournament-visualization/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  wsEndpoint: 'ws://localhost:8765',
   dataEndpoint: 'http://localhost:5140/data/', // testrunde1/run-20220424-214441-0.json
   timeEndpoint: 'http://localhost:5140/get-time/',
   reconnectInterval: 2000
