c9d54dfe278ce7265cc33626b785933d4b5cd81b
Christian Alexander Hetling <chrhe@itu.dk>
2023-04-14 14:06:16 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  dataEndpoint: 'http://pokerbot.dk/api/data/', // testrunde1/run-20220424-214441-0.json
-  timeEndpoint: 'http://pokerbot.dk/api/get-time/',
+  dataEndpoint: 'https://pokerbot.dk/api/data/', // testrunde1/run-20220424-214441-0.json
+  timeEndpoint: 'https://pokerbot.dk/api/get-time/',
   reconnectInterval: 2000
 };
