e443eceefb8065797966c37ca72b728d991c6b21
Christian Alexander Hetling <chrhe@itu.dk>
2024-03-01 13:14:45 +0100

    change endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  dataEndpoint: 'http://localhost:5140/data/', // testrunde1/run-20220424-214441-0.json
-  timeEndpoint: 'http://localhost:5140/get-time/',
+  dataEndpoint: 'https://api.pokerbot.dk/data/', // testrunde1/run-20220424-214441-0.json
+  timeEndpoint: 'https://api.pokerbot.dk/get-time/',
   reconnectInterval: 2000
 };
 
