96e89fe71d632edcb2b3cd57e72d6366342d8b3e
Frederik Petersen <fred.p@live.dk>
2024-03-01 10:17:51 +0100

    Change data endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  dataEndpoint: 'https://pokerbot.dk/api/data/', // testrunde1/run-20220424-214441-0.json
-  timeEndpoint: 'https://pokerbot.dk/api/get-time/',
-  reconnectInterval: 2000
+  dataEndpoint: 'https://api.pokerbot.dk/data/', // testrunde1/run-20220424-214441-0.json
+  timeEndpoint: 'https://api.pokerbot.dk/get-time/',
+  reconnectInterval: 2000,
 };
