59b73832ae5b9b0d4614dd56d930cb1a32198dd4
Frederik Petersen <fred.p@live.dk>
2025-02-14 16:22:29 +0100

    Fix start time

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,13 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const baseUrl = 'https://api.pokerbot.dk';
+
 export const environment = {
   production: false,
-  dataEndpoint: 'https://api.pokerbot.dk/data/', // testrunde1/run-20220424-214441-0.json
-  timeEndpoint: 'https://api.pokerbot.dk/get-time',
-  botEndpoint: 'https://api.pokerbot.dk/bots/',
+  dataEndpoint: `${baseUrl}/data/`, // testrunde1/run-20220424-214441-0.json
+  timeEndpoint: `${baseUrl}/get-time`,
+  botEndpoint: `${baseUrl}/bots/`,
   reconnectInterval: 2000,
 };
 
