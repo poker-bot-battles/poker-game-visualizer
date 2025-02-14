256ceda050b2d201d362a3f00da4e081a3954a08
Frederik Petersen <fred.p@live.dk>
2025-02-14 15:41:58 +0100

    Sync bots from api every 5 seconds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   dataEndpoint: 'https://api.pokerbot.dk/data/', // testrunde1/run-20220424-214441-0.json
   timeEndpoint: 'https://api.pokerbot.dk/get-time',
+  botEndpoint: 'https://api.pokerbot.dk/bots/',
   reconnectInterval: 2000,
 };
 
